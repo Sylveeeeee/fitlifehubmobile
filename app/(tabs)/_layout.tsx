1fa9535f313b4985f28c55ca16dc357e4e004176
jirawat <66022477@up.ac.th>
2025-07-24 00:51:27 +0700

    food id

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -5,10 +5,14 @@ import BottomNavbar from '@/components/BottomNavbar';
 import "@/global.css";
 
 export default function RootLayout() {
+  const pathname = usePathname();
+  // ซ่อน BottomNavbar เมื่ออยู่หน้า foods/[id]
+  const showNavbar = !(pathname.startsWith('/(tabs)/foods/'));
+
   return (
     <>
       <Stack screenOptions={{ headerShown: false }} />
-      <BottomNavbar />
+      {showNavbar && <BottomNavbar />}
       <StatusBar style="auto" />
     </>
   );
