04354f0e700f99c580748e057003ca7b19e5f51e
jirawat <66022477@up.ac.th>
2025-07-24 00:33:07 +0700

    layout

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -5,14 +5,10 @@ import BottomNavbar from '@/components/BottomNavbar';
 import "@/global.css";
 
 export default function RootLayout() {
-  const pathname = usePathname();
-  // เงื่อนไข: ถ้าอยู่หน้า foods/[id] จะไม่แสดง BottomNavbar
-  const showNavbar = !(pathname.startsWith('/(tabs)/foods/[id]') && pathname !== '/(tabs)/foods/[id]');
-
   return (
     <>
       <Stack screenOptions={{ headerShown: false }} />
-      {showNavbar && <BottomNavbar />}
+      <BottomNavbar />
       <StatusBar style="auto" />
     </>
   );
